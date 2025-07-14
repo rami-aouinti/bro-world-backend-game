6812ed8eb0d9b43a3b8b9f9586e7e2d757d12cfe
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-07-14 03:32:53 +0200

    add fehlende Temporal Config Tags

--- src/Quiz/Infrastructure/DataFixtures/ORM/CategoryLevelFixtures.php
+++ src/Quiz/Infrastructure/DataFixtures/ORM/CategoryLevelFixtures.php
@@ -2,33 +2,62 @@
 
 namespace App\Quiz\Infrastructure\DataFixtures\ORM;
 
+use App\Quiz\Domain\Entity\Answer;
 use App\Quiz\Domain\Entity\Category;
 use App\Quiz\Domain\Entity\Level;
+use App\Quiz\Domain\Entity\Question;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Random\RandomException;
+use Faker\Factory;
 
 class CategoryLevelFixtures extends Fixture
 {
+    /**
+     * @throws RandomException
+     */
     public function load(ObjectManager $manager): void
     {
-        $categories = [
-            'Culture', 'Sciences', 'Sport', 'Histoire', 'Géographie', 'Technologie', 'Langues'
-        ];
+        $faker = Factory::create();
 
-        foreach ($categories as $catName) {
+        // Create Categories
+        $categories = ['Culture', 'Science', 'Geography', 'History'];
+        $categoryEntities = [];
+
+        foreach ($categories as $cat) {
             $category = new Category();
-            $category->setName($catName);
+            $category->setName($cat);
             $manager->persist($category);
+            $categoryEntities[] = $category;
         }
 
-        $levels = [
-            'Facile', 'Moyen', 'Difficile'
-        ];
+        // Create Levels
+        $levels = ['Easy', 'Medium', 'Hard'];
+        $levelEntities = [];
 
         foreach ($levels as $label) {
             $level = new Level();
             $level->setLabel($label);
             $manager->persist($level);
+            $levelEntities[] = $level;
+        }
+
+        // Create Questions and Answers
+        for ($i = 1; $i <= 20; $i++) {
+            $question = new Question();
+            $question->setQuestion($faker->sentence(6));
+            $question->setCategory($faker->randomElement($categoryEntities));
+            $question->setLevel($faker->randomElement($levelEntities));
+            $manager->persist($question);
+
+            $correctAnswerIndex = random_int(0, 3);
+            for ($j = 0; $j < 4; $j++) {
+                $answer = new Answer();
+                $answer->setAnswer($faker->word());
+                $answer->setIsTrue($j === $correctAnswerIndex);
+                $answer->setQuestionId($question);
+                $manager->persist($answer);
+            }
         }
 
         $manager->flush();
